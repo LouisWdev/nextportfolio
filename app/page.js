663ee0b7d04d124ca61e3b60c5bc8cda5e2af8b1
Louis <louis.wackenier@gmail.com>
2024-09-06 08:24:32 +0200

    Added main page and footer component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 
+import Main from './Pages/Main';
+
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+         <Home />
+      <Footer />
     </div>
       
   );
