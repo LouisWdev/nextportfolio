fbaf0e1fa22c8fdf5c286b0b65f552bb05ed4f42
Louis <louis.wackenier@gmail.com>
2024-09-17 08:11:08 +0200

    imports

--- app/page.js
+++ app/page.js
@@ -4,12 +4,16 @@ import Footer from './components/footer';
 
 // PAGES
 import Main from './Pages/Main';
+import Portfolio from './Pages/Portfolio';
+import Contact from './Pages/Contact';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
          <Main />
+         <Portfolio />
+         <Contact />
       <Footer />
     </div>
       
