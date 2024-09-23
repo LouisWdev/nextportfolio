4b51c3ad96e6f8763fb6de83feaa34e30f6dd7e9
Louis <louis.wackenier@gmail.com>
2024-09-23 08:22:42 +0200

    Main page

--- app/Pages/Main.js
+++ app/Pages/Main.js
@@ -1,8 +1,8 @@
 const Main = () => {
   return (
   <>
-    <h1>Main</h1>
-    <h2>This is gonna be the main</h2>
+    <h1>Welcome to my portfolio</h1>
+    <h2>Feel freee to look around my portfolio !</h2>
   </>
   )
 }
