27d9540911e45c75ea41932f058f78db6c7556d7
Louis <louis.wackenier@gmail.com>
2024-09-27 05:54:07 +0200

    About page

--- app/Pages/About.js
+++ app/Pages/About.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <h2>The about me page</h2>
+    <>
+      <h2>The about me page</h2>
+        <h3>Why I started coding</h3>
+        <p></p>
+    </>
   )
 }
 
