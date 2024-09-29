1c7c581cf130edb7b5ac8ee8f237b205774351bd
Louis <louis.wackenier@gmail.com>
2024-09-29 08:27:17 +0200

    About page

--- app/Pages/About.js
+++ app/Pages/About.js
@@ -6,6 +6,8 @@ const About = () => {
       <h2>The about me page</h2>
         <h3>Why I started coding</h3>
         <p>The reason why I started is because of interests and other factors.</p>
+
+        <h3>AI</h3>
     </>
   )
 }
