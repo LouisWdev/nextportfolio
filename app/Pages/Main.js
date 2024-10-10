0c9aa2ca03536d81dc4d582f728dd6505343ebda
Louis <louis.wackenier@gmail.com>
2024-10-10 06:42:35 +0200

    Main

--- app/Pages/Main.js
+++ app/Pages/Main.js
@@ -1,10 +1,18 @@
+import styled from "styled-components"
+
 const Main = () => {
   return (
   <>
     <h1>Welcome to my portfolio</h1>
     <h2>Feel free to look around my portfolio !</h2>
+
+    <PortfolioButton></PortfolioButton>
   </>
   )
 }
 
 export default Main
+
+const PortfolioButton = styled.button`
+
+`;
