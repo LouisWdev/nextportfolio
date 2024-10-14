30951cf4be70d423a167a8c706b42b5d0ee942ad
Louis <louis.wackenier@gmail.com>
2024-10-14 08:52:02 +0200

    Buttons

--- app/Pages/Main.js
+++ app/Pages/Main.js
@@ -6,7 +6,13 @@ const Main = () => {
     <h1>Welcome to my portfolio</h1>
     <h2>Feel free to look around my portfolio !</h2>
 
-    <PortfolioButton>Go to Portfolio</PortfolioButton>
+    <Buttons>
+      <PortfolioButton>My Portfolio</PortfolioButton>
+      <ContactButton>Contact Me</ContactButton>
+      <AboutButton>About Me</AboutButton>
+    </Buttons>
+
+
   </>
   )
 }
