3bac686311098b34a1db180b1ba54685bc863aa2
Louis <louis.wackenier@gmail.com>
2024-10-17 12:28:49 +0200

    Portfolio section

--- app/Pages/Portfolio.js
+++ app/Pages/Portfolio.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const Portfolio = () => {
   return (
+    
     <PortfolioContainer>
         <h3>Project name</h3>   
         <p>Project description</p>
@@ -16,3 +17,4 @@ export default Portfolio
 const PortfolioContainer = styled.div``;
 
 const PortfolioTitle = styled.h2``;
+const SubText = styled.h3``;
