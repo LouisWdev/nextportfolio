a967b88c9b9bfecc862455718430a3a9f2b1ef62
Louis <louis.wackenier@gmail.com>
2024-10-22 07:56:02 +0200

    Portfolio section

--- app/Pages/Portfolio.js
+++ app/Pages/Portfolio.js
@@ -16,6 +16,7 @@ const Portfolio = () => {
       <PortfolioContainer>
           <h3>Project name</h3>   
           <p>Project description</p>
+          <Image alt=""/>
           <button>Go to</button>
       </PortfolioContainer>
       </>
